cce5bd2132461719a12c988d9efcde862225639d
Jagankumarg <jagan0523@gmail.com>
2023-11-21 18:15:45 +0530

    RestController update

--- src/main/java/com/spring/rest/SpringRestTestApplication.java
+++ src/main/java/com/spring/rest/SpringRestTestApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringRestTestApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringRestTestApplication.class, args);
+		//
 	}
 
 }
